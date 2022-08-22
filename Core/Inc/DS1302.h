445db41aa8218169be538b037a3f2590b710f19c
hyperdma1 <110908259+hyperdma1@users.noreply.github.com>
2022-08-22 10:20:22 +0300

    Update DS1302.h

--- Core/Inc/DS1302.h
+++ Core/Inc/DS1302.h
@@ -13,22 +13,23 @@
 
 typedef enum
 {
-	SUNDAY = 1,
-	MONDAY = 2,
-	TUESDAY = 3,
-	WEDNESDAY = 4,
-	THURSDAY = 5,
-	FRIDAY = 6,
-	SATURDAY = 7
+	SUNDAY =         1,
+	MONDAY =         2,
+	TUESDAY =        3,
+	WEDNESDAY =      4,
+	THURSDAY =       5,
+	FRIDAY =         6,
+	SATURDAY =       7
 } days;
 
 /* Initialization */
-/* GPIO and DWT */
-void DS1302_Init(void);
-
+/* Conversion */
 uint8_t DEC2BCD(uint8_t dec);
 
 uint8_t BCD2DEC(uint8_t bcd);
+
+/* GPIO */
+void DS1302_Init(void);
 
 /* Reads time byte by byte to 'buf' */
 void DS1302_ReadTime(uint8_t *buf);
